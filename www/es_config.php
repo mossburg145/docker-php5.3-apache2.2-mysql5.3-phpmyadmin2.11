7941ebda436adadf6958dbdfa25e23b9506dc569
mossburg145 <mossburg@aol.com>
2018-12-08 02:21:07 -0500

    Update es_config.php

--- www/es_config.php
+++ www/es_config.php
@@ -17,7 +17,7 @@ if (strstr($_SERVER["PHP_SELF"],"config.php")) {
 
 // ---- Define global variables
 //- Database Connection variables 
-$DBHOST = "172.16.238.11";			//- MySQL Database Host
+$DBHOST = "localhost";			//- MySQL Database Host
 $DBNAME = "scheduler";		//- MySQL Database Name
 $DBUSER = "username";			//- MySQL DB Username
 $DBPASS = "password";			//- MySQL DB Userpassword
